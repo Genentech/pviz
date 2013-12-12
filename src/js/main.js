e73aee98d9f78c0d66d0b1af2e4d2971ac9021a7
mierzwid <mierzwid@mierzwid.fake>
2013-12-12 15:26:23 +0100

    bower based requirejs config

--- src/js/main.js
+++ src/js/main.js
@@ -3,33 +3,44 @@ requirejs.config({
     paths: {
         pviz: '.',
         pviz_templates: '../templates',
-		jquery: '../../bower_components/jquery/jquery',
+        jquery: '../../bower_components/jquery/jquery',
         bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap',
-		requirejs: '../../bower_components/requirejs/require',
-        'domReady': '../../bower_components/requirejs-domready/domReady',
-        'text': '../../bower_components/requirejs-text/text',
-        'd3': '../../bower_components/d3-amd/d3',
-        'backbone': '../../bower_components/backbone-amd/backbone',
-        'underscore': '../../bower_components/underscore-amd/underscore'
+        requirejs: '../../bower_components/requirejs/require',
+        'backbone-amd': '../../bower_components/backbone-amd/backbone',
+        'd3-amd': '../../bower_components/d3-amd/d3',
+        'requirejs-domready': '../../bower_components/requirejs-domready/domReady',
+        'requirejs-text': '../../bower_components/requirejs-text/text',
+        'underscore-amd': '../../bower_components/underscore-amd/underscore'
+    },
+    map: {
+        '*': {
+            text: 'requirejs-text',
+            domReady: 'requirejs-domready',
+            underscore: 'underscore-amd',
+            backbone: 'backbone-amd',
+            d3: 'd3-amd'
+        }
     },
     shim: {
         jquery: {
             exports: '$'
         },
-		bootstrap : {
-            deps: ['jquery']
+        bootstrap: {
+            deps: [
+                'jquery'
+            ]
         },
-        underscore: {
+        'underscore-amd': {
             exports: '_'
         },
-        backbone: {
+        'backbone-amd': {
             deps: [
-                'underscore',
+                'underscore-amd',
                 'jquery'
             ],
             exports: 'Backbone'
         },
-        'd3': {
+        'd3-amd': {
             exports: 'd3'
         }
     }
