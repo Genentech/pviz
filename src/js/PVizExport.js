e01c831290839009d4698f7b30f7e00aed5572a0
mierzwid <mierzwid@mierzwid.fake>
2013-12-16 15:13:45 +0100

    pViz as adm module - cleanup

--- src/js/PVizExport.js
+++ src/js/PVizExport.js
@@ -3,7 +3,7 @@
  * Authors: Alexandre Masselot, Kiran Mukhyala, Bioinformatics & Computational Biology
  */
 var classPaths = ['pviz/models/SeqEntry', 'pviz/services/DASReader', 'pviz/services/FastaReader', 'pviz/services/FeatureManager', 'pviz/services/IconFactory', 'pviz/views/SeqEntryAnnotInteractiveView', 'pviz/views/SeqEntryFastaView', 'pviz/views/FeatureDisplayer', 'pviz/views/OneLiner']
-define(['pviz/models/SeqEntry', 'pviz/services/DASReader', 'pviz/services/FastaReader', 'pviz/services/FeatureManager', 'pviz/services/IconFactory', 'pviz/views/SeqEntryAnnotInteractiveView', 'pviz/views/SeqEntryFastaView', 'pviz/views/FeatureDisplayer', 'pviz/views/OneLiner'], function() {
+define(['pviz/models/SeqEntry', 'pviz/services/DASReader', 'pviz/services/FastaReader', 'pviz/services/FeatureManager', 'pviz/services/IconFactory', 'pviz/views/SeqEntryAnnotInteractiveView', 'pviz/views/SeqEntryFastaView', 'pviz/views/FeatureDisplayer', 'pviz/views/OneLiner'], function(SeqEntry, DASReader, FastaReader, FeatureManager, IconFactory, SeqEntryAnnotInteractiveView, SeqEntryFastaView, FeatureDisplayer, OneLiner) {
   var args = arguments;
   var exp = {}
   for (i in classPaths) {
