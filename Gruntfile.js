27ade840024451f4993eb0185a1270b416443e06
mierzwid <mierzwid@mierzwid.fake>
2013-12-16 17:33:22 +0100

    concatenating copyright to created files

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('bower.json'),
 		src_files: ['src/js/**/*.js', '!src/js/*.js'],
+		copyright: "build-tools/copyright.txt",
 		uglify: {
 		  options: {
 			banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -73,7 +74,26 @@ module.exports = function(grunt) {
 					baseUrl: "src/js",
 					mainConfigFile: "src/js/main.js",
 					out: "build/<%= pkg.name %>-amd.min.js",
-					exclude: ['jquery', 'underscore', 'backbone', 'd3', 'bootstrap', 'domReady', 'text']
+					exclude: ['jquery', 'underscore', 'backbone', 'd3', 'bootstrap', 'domReady', 'text'],
+					wrap: {
+						endFile: "build-tools/amd-module-end.txt"
+					},
+					preserveLicenseComments: false
+				}
+			}
+		},
+		concat: {
+			options: {
+				separator: '\n',
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			concat_copyright:{
+				files: {
+					'build/<%= pkg.name %>-amd.js': ["<%= copyright %>", 'build/<%= pkg.name %>-amd.js'],
+					'build/<%= pkg.name %>-amd.min.js': ["<%= copyright %>", 'build/<%= pkg.name %>-amd.min.js'],
+					'build/<%= pkg.name %>-bundle.js': ["<%= copyright %>", 'build/<%= pkg.name %>-bundle.js'],
+					'build/<%= pkg.name %>-bundle.min.js': ["<%= copyright %>", 'build/<%= pkg.name %>-bundle.min.js']
 				}
 			}
 		},
@@ -91,8 +111,10 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-bower-requirejs');
 	grunt.loadNpmTasks('grunt-requirejs');
 	grunt.loadNpmTasks('grunt-devserver');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	
 	// Default task(s).
 	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('build', ['requirejs', 'concat:concat_copyright']);
 
 };
--- build/pviz-bundle.js
+++ build/pviz-bundle.js
@@ -1,3 +1,22 @@
+/*! pviz - v0.1.1 - 2013-12-16 */
+/**
+	* pViz
+	* Copyright (c) 2013, Genentech Inc.
+	* All rights reserved.
+	* Authors: Alexandre Masselot, Kiran Mukhyala, Bioinformatics & Computational Biology, Genentech
+	Redistribution and use in source and binary forms are permitted
+	provided that the above copyright notice and this paragraph are	
+	duplicated in all such forms and that any documentation,
+	advertising materials, and other materials related to such
+	distribution and use acknowledge that the software was developed
+	by the Bioinformatics and Computational Biology Department, Genentech Inc.  The name of
+	Genentech may not be used to endorse or promote products derived
+	from this software without specific prior written permission.
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+	IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
 
 /*!
  * jQuery JavaScript Library v1.8.3
